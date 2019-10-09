635ae40b915be623da127b5f09dda44ff1146958
Forrest Hampton <forrest.hampton744@myci.csuci.edu>
2019-10-09 16:30:40 -0700

    Added close button function and score text

--- app/src/main/java/edu/csuci/appaca/activities/FruitCatchActivity.java
+++ app/src/main/java/edu/csuci/appaca/activities/FruitCatchActivity.java
@@ -3,6 +3,7 @@ package edu.csuci.appaca.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import edu.csuci.appaca.R;
 
@@ -12,5 +13,10 @@ public class FruitCatchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fruit_catch);
+    }
+
+    public void close(View view)
+    {
+        setContentView(R.layout.activity_main);
     }
 }
