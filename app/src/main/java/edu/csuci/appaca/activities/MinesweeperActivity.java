6375574511c988851f3d5fa4ef7a37b07d29cfbc
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2019-10-09 14:05:52 -0700

    Created Activity Files

--- app/src/main/java/edu/csuci/appaca/activities/MinesweeperActivity.java
+++ app/src/main/java/edu/csuci/appaca/activities/MinesweeperActivity.java
@@ -1,14 +1,16 @@
-package edu.csuci.appaca;
+package edu.csuci.appaca.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import edu.csuci.appaca.R;
+
+public class MinesweeperActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_minesweeper);
     }
 }
---
+++ app/src/main/java/edu/csuci/appaca/activities/ShopActivity.java
@@ -0,0 +1,16 @@
+package edu.csuci.appaca.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.csuci.appaca.R;
+
+public class ShopActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop);
+    }
+}
