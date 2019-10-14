a99fbe4e43f8e0e7f6a9afc197355b37528ee8d0
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2019-10-14 14:45:18 -0700

    Button clicks

--- app/src/main/java/edu/csuci/appaca/activities/MainActivity.java
+++ app/src/main/java/edu/csuci/appaca/activities/MainActivity.java
@@ -2,7 +2,11 @@ package edu.csuci.appaca.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import edu.csuci.appaca.R;
 
@@ -12,5 +16,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initButtons();
     }
+
+    private void initButtons() {
+        final Button shopBtn = findViewById(R.id.shopBtn);
+        final Button playBtn = findViewById(R.id.playBtn);
+
+        shopBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ShopActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        playBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MinigameSelectActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
 }
