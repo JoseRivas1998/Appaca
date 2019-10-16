935c8aeaf5a29fd7217f21ef57688f999f4a1348
LeeSolomon <Leesolomon808@gmail.com>
2019-10-16 00:01:19 -0700

    Clothing Selection is dooone
    Yeet

--- app/src/main/java/edu/csuci/appaca/activities/ClothingSelectActivity.java
+++ app/src/main/java/edu/csuci/appaca/activities/ClothingSelectActivity.java
@@ -1,10 +1,13 @@
 package edu.csuci.appaca.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import edu.csuci.appaca.R;
+
 
 public class ClothingSelectActivity extends AppCompatActivity {
 
@@ -17,7 +20,80 @@ public class ClothingSelectActivity extends AppCompatActivity {
     public void initButton()
     {
         final Button exitButton = findViewById(R.id.exitButton);
+        final Button button3 = findViewById(R.id.button3);
+        final Button button4 = findViewById(R.id.button4);
+        final Button button5 = findViewById(R.id.button5);
+        final Button button6 = findViewById(R.id.button6);
+        final Button button7 = findViewById(R.id.button7);
+        final Button button8 = findViewById(R.id.button8);
+        final Button button9 = findViewById(R.id.button9);
+        final Button button10 = findViewById(R.id.button10);
+        final Button button11 = findViewById(R.id.button11);
+
         exitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ClothingSelectActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ClothingSelectActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ClothingSelectActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ClothingSelectActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ClothingSelectActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ClothingSelectActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        button8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ClothingSelectActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        button9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ClothingSelectActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        button10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ClothingSelectActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        button11.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(ClothingSelectActivity.this, MainActivity.class);
@@ -28,4 +104,4 @@ public class ClothingSelectActivity extends AppCompatActivity {
     }
 }
 
-}
+
