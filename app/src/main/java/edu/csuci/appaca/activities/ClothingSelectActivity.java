3f24d1185580a7f46c05b7cbd2c79c0cd98398d7
LeeSolomon <Leesolomon808@gmail.com>
2019-10-15 23:21:26 -0700

    Just saving it really quick
    bottom text

---
+++ app/src/main/java/edu/csuci/appaca/activities/ClothingSelectActivity.java
@@ -0,0 +1,18 @@
+package edu.csuci.appaca.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.csuci.appaca.R;
+
+public class ClothingSelectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clothing_select);
+    }
+
+
+}
