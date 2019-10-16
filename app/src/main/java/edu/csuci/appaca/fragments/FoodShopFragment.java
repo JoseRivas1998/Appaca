b2bb7444dfce3c24fb7f181d9574d4f010442ac3
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2019-10-15 22:01:53 -0700

    Populate Shop Grid

--- app/src/main/java/edu/csuci/appaca/fragments/FoodShopFragment.java
+++ app/src/main/java/edu/csuci/appaca/fragments/FoodShopFragment.java
@@ -3,13 +3,18 @@ package edu.csuci.appaca.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridLayout;
+import android.widget.ImageView;
 
 import edu.csuci.appaca.R;
+import edu.csuci.appaca.utils.ScreenUtils;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -29,4 +34,20 @@ public class FoodShopFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_food_shop, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        final GridLayout gridLayout = view.findViewById(R.id.food_shop_grid);
+        int size = (int) ScreenUtils.dpToPixels(view.getContext(), 100);
+        int margin = (int) ScreenUtils.dpToPixels(view.getContext(), 20);
+        for (int i = 0; i < 9; i++) {
+            ImageView appleView = new ImageView(this.getContext());
+            appleView.setImageResource(R.drawable.apple);
+            GridLayout.LayoutParams params = new GridLayout.LayoutParams();
+            params.width = size;
+            params.height = size;
+            params.setMargins(margin, margin, margin, margin);
+            appleView.setLayoutParams(params);
+            gridLayout.addView(appleView);
+        }
+    }
 }
