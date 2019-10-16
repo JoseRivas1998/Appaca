75af2f7b6c6255662f5a087cb9cc834e0a66b43f
teagan <teagan.mail@gmail.com>
2019-10-15 21:26:39 -0700

    Close matching game

--- app/src/main/java/edu/csuci/appaca/activities/MatchingActivity.java
+++ app/src/main/java/edu/csuci/appaca/activities/MatchingActivity.java
@@ -1,16 +1,33 @@
 package edu.csuci.appaca.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import edu.csuci.appaca.R;
 
 public class MatchingActivity extends AppCompatActivity {
 
+    private ImageButton returnToMain;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_matching);
+
+        returnToMain = (ImageButton) findViewById(R.id.exitButton);
+
+        returnToMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
+
+
+
+
