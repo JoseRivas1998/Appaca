f3fac2640513bbd530cda096364a3eb39eab6c27
Forrest Hampton <forrest.hampton744@myci.csuci.edu>
2019-10-21 14:23:55 -0700

    Food Depletion Algorithm V1

--- app/src/main/java/edu/csuci/appaca/data/FoodDepletion.java
+++ app/src/main/java/edu/csuci/appaca/data/FoodDepletion.java
@@ -2,8 +2,16 @@ package edu.csuci.appaca.data;
 
 public class FoodDepletion extends Thread
 {
-    public double foodDepletion(double currentFood, int secondsTilNextCall)
+    public double foodDepletion(double currentFood)
     {
-        return 0.0;
+        //Assume that this function is called every second
+        final int NUM_HOURS_TIL_DEPLETED = 4; //assuming it takes 4 hours til the hunger is fully depleted
+        final double MAX_FOOD = 1.0; //assume that the max hunger of any alpaca is 1.0
+        final int MINPERHOUR = 60;
+        final int SECPERMIN = 60;
+        final double DECREMENTVAL = (MAX_FOOD/(NUM_HOURS_TIL_DEPLETED * MINPERHOUR * SECPERMIN));
+
+        //return the new food value after loss
+        return (currentFood - DECREMENTVAL);
     }
 }
