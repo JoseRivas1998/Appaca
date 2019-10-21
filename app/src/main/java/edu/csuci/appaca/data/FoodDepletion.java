490b37e2d556757e1afb1238ffa488a832242ea6
Forrest Hampton <forrest.hampton744@myci.csuci.edu>
2019-10-21 15:51:44 -0700

    Added time utility stub and updated food depletion function

--- app/src/main/java/edu/csuci/appaca/data/FoodDepletion.java
+++ app/src/main/java/edu/csuci/appaca/data/FoodDepletion.java
@@ -1,10 +1,15 @@
 package edu.csuci.appaca.data;
 
-public class FoodDepletion extends Thread
+import edu.csuci.appaca.utils.TimeUtils;
+
+public class FoodDepletion
 {
-    public double foodDepletion(Alpaca alpaca, long previousTime, long currentTime)
+    public double foodDepletion(Alpaca alpaca, long previousTime)
     {
         final double FOOD_LOSS_PER_MINUTE = 0.015; //how much food is decremented by per minute
+
+        TimeUtils clock = new TimeUtils();
+        long currentTime = clock.getCurrentTime();
 
         long timeInMinutes = (currentTime - previousTime) * 60; //get difference from current and previous timestamp and convert to minutes
 
---
+++ app/src/main/java/edu/csuci/appaca/utils/TimeUtils.java
@@ -0,0 +1,9 @@
+package edu.csuci.appaca.utils;
+
+public class TimeUtils {
+
+    public long getCurrentTime()
+    {
+        return 0; //stub
+    }
+}
