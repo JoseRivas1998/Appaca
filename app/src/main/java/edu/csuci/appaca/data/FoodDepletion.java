90e3dda8bf07f06d12ad9596d3bb860c16cab415
Forrest Hampton <forrest.hampton744@myci.csuci.edu>
2019-10-21 14:10:08 -0700

    Food Depletion Class

---
+++ app/src/main/java/edu/csuci/appaca/data/FoodDepletion.java
@@ -0,0 +1,9 @@
+package edu.csuci.appaca.data;
+
+public class FoodDepletion extends Thread
+{
+    public double foodDepletion(double currentFood, int secondsTilNextCall)
+    {
+        return 0.0;
+    }
+}
