6a2924374d82e71ed7353abefc951c3ec5a3e7dc
Forrest Hampton <forrest.hampton744@myci.csuci.edu>
2019-10-22 15:26:20 -0700

    Changed to UTC

--- app/src/main/java/edu/csuci/appaca/utils/TimeUtils.java
+++ app/src/main/java/edu/csuci/appaca/utils/TimeUtils.java
@@ -1,6 +1,8 @@
 package edu.csuci.appaca.utils;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.TimeZone;
 
 public class TimeUtils {
 
@@ -9,7 +11,9 @@ public class TimeUtils {
      */
     static public long getCurrentTime() {
         final int MILLISECONDS_IN_SECONDS = 1000;
-        Date clock = new Date();
+        Calendar date = Calendar.getInstance();
+        date.setTimeZone(TimeZone.getTimeZone("UTC")); //change to UTC
+        Date clock = date.getTime(); //get current time from the UTC calendar
         long currentTime = clock.getTime(); //gets milliseconds since epoch
         currentTime /= MILLISECONDS_IN_SECONDS; //turns milliseconds to seconds
         return currentTime;
