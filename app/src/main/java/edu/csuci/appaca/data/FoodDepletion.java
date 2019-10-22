2f02527dd0780d90584350e9579e48236658eaa6
Forrest Hampton <forrest.hampton744@myci.csuci.edu>
2019-10-22 09:34:47 -0700

    Constant food depletion over 8 hours

--- app/src/main/java/edu/csuci/appaca/data/FoodDepletion.java
+++ app/src/main/java/edu/csuci/appaca/data/FoodDepletion.java
@@ -3,16 +3,21 @@ package edu.csuci.appaca.data;
 import edu.csuci.appaca.utils.TimeUtils;
 
 public class FoodDepletion {
-    public double foodDepletion(Alpaca alpaca, long previousTime) {
-        final double FOOD_LOSS_PER_MINUTE = 0.015; //how much food is decremented by per minute
 
-        long currentTime = TimeUtils.getCurrentTime();
-
-        long timeInMinutes = (currentTime - previousTime) * 60; //get difference from current and previous timestamp and convert to minutes
+    /*
+        Alpaca food drain is calculated based on the how much food the alpaca currently has:
+        If less than 75%, the rate is doubled
+        If less than 50%, the rate is tripled
+        If less than 25%, the rate is quadrupled
+     */
+    public static double foodDepletion(Alpaca alpaca, long previousTime, double maxHunger) {
+        final int TIME_TIL_FULLY_DEPLETED = 8 * 60; //8 hours * 60 minutes
 
         double alpacaFood = alpaca.getFoodStat();
+        long currentTime = TimeUtils.getCurrentTime();
+        long timeInMinutes = (currentTime - previousTime) * 60; //get difference from current and previous timestamp and convert to minutes
 
-        //return the new food loss based on time and loss per minute
-        return (alpacaFood - (timeInMinutes * FOOD_LOSS_PER_MINUTE));
+        //return the new food loss based on time, max hunger, and time til fully depleted
+        return (alpacaFood - (timeInMinutes / (maxHunger / TIME_TIL_FULLY_DEPLETED)));
     }
 }
