1a2aa52d275a7ecf644aa8e1cf990153a669caec
Forrest Hampton <forrest.hampton744@myci.csuci.edu>
2019-10-22 09:57:59 -0700

    Dynamic depletion over 8 hours

--- app/src/main/java/edu/csuci/appaca/data/FoodDepletion.java
+++ app/src/main/java/edu/csuci/appaca/data/FoodDepletion.java
@@ -6,18 +6,25 @@ public class FoodDepletion {
 
     /*
         Alpaca food drain is calculated based on the how much food the alpaca currently has:
-        If less than 75%, the rate is doubled
-        If less than 50%, the rate is tripled
-        If less than 25%, the rate is quadrupled
+        Normal rate at 100% - 75%, stage 1
+        If less than 75%, the rate is doubled, stage 2
+        If less than 50%, the rate is tripled, stage 3
+        If less than 25%, the rate is quadrupled, stage 4
      */
     public static double foodDepletion(Alpaca alpaca, long previousTime) {
-        final int TIME_TIL_FULLY_DEPLETED = 8 * 60; //8 hours * 60 minutes
+        final int TIME_TIL_FULLY_DEPLETED = (8 * 60)/4; //8 hours * 60 minutes over 4 stages
 
         double alpacaFood = alpaca.getFoodStat();
         long currentTime = TimeUtils.getCurrentTime();
         long timeInMinutes = (currentTime - previousTime) * 60; //get difference from current and previous timestamp and convert to minutes
+        double percentFood = (alpacaFood/Alpaca.MAX_STAT);
+        int stageModifier = 1; //modifier for stage 1
 
-        //return the new food loss based on time, max hunger, and time til fully depleted
-        return (alpacaFood - (timeInMinutes / (Alpaca.MAX_STAT / TIME_TIL_FULLY_DEPLETED)));
+        if (percentFood < 0.75 && percentFood >= 0.5){stageModifier = 2;} //stage 2
+        else if (percentFood <0.5 && percentFood >= 0.25){stageModifier = 3;} //stage 3
+        else {stageModifier = 4;} //stage 4
+
+        //return the new food loss based on time, max hunger, time til fully depleted, and percent of
+        return (alpacaFood - (timeInMinutes / (Alpaca.MAX_STAT / (TIME_TIL_FULLY_DEPLETED * stageModifier))));
     }
 }
