c6ce939a5b5899230b593498a00d60aaaed9dda2
Forrest Hampton <forrest.hampton744@myci.csuci.edu>
2019-10-22 15:13:39 -0700

    No more magic numbers

--- app/src/main/java/edu/csuci/appaca/utils/TimeUtils.java
+++ app/src/main/java/edu/csuci/appaca/utils/TimeUtils.java
@@ -8,9 +8,10 @@ public class TimeUtils {
         Gets current time as a measure of seconds since epoch
      */
     static public long getCurrentTime() {
+        final int MILLISECONDS_IN_SECONDS = 1000;
         Date clock = new Date();
         long currentTime = clock.getTime(); //gets milliseconds since epoch
-        currentTime /= 1000; //turns milliseconds to seconds
+        currentTime /= MILLISECONDS_IN_SECONDS; //turns milliseconds to seconds
         return currentTime;
     }
 }
