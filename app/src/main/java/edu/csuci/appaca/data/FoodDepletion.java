ff9abe6deeb0f24acf51a20ec7691d5329111fad
Forrest Hampton <forrest.hampton744@myci.csuci.edu>
2019-10-22 09:36:31 -0700

    Used max stat

--- app/src/main/java/edu/csuci/appaca/data/FoodDepletion.java
+++ app/src/main/java/edu/csuci/appaca/data/FoodDepletion.java
@@ -10,7 +10,7 @@ public class FoodDepletion {
         If less than 50%, the rate is tripled
         If less than 25%, the rate is quadrupled
      */
-    public static double foodDepletion(Alpaca alpaca, long previousTime, double maxHunger) {
+    public static double foodDepletion(Alpaca alpaca, long previousTime) {
         final int TIME_TIL_FULLY_DEPLETED = 8 * 60; //8 hours * 60 minutes
 
         double alpacaFood = alpaca.getFoodStat();
@@ -18,6 +18,6 @@ public class FoodDepletion {
         long timeInMinutes = (currentTime - previousTime) * 60; //get difference from current and previous timestamp and convert to minutes
 
         //return the new food loss based on time, max hunger, and time til fully depleted
-        return (alpacaFood - (timeInMinutes / (maxHunger / TIME_TIL_FULLY_DEPLETED)));
+        return (alpacaFood - (timeInMinutes / (Alpaca.MAX_STAT / TIME_TIL_FULLY_DEPLETED)));
     }
 }
