d4cb0c2f9f207cb14c89f0b7904934c18b23dcfb
Forrest Hampton <forrest.hampton744@myci.csuci.edu>
2019-10-23 14:31:01 -0700

    HappinessCalc class

---
+++ app/src/main/java/edu/csuci/appaca/data/HappinessCalc.java
@@ -0,0 +1,10 @@
+package edu.csuci.appaca.data;
+
+import edu.csuci.appaca.data.statics.StaticClothesItem;
+
+public class HappinessCalc {
+    static public double calcHappiness(StaticClothesItem clothes, Alpaca alpaca, long currentTime)
+    {
+        return 0.0; //stub
+    }
+}
