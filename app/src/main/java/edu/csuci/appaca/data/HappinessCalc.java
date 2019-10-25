0f48eeacdb2dff6a134325d79e92c864a552a06e
Forrest Hampton <forrest.hampton744@myci.csuci.edu>
2019-10-25 10:38:11 -0700

    Javadoc

--- app/src/main/java/edu/csuci/appaca/data/HappinessCalc.java
+++ app/src/main/java/edu/csuci/appaca/data/HappinessCalc.java
@@ -6,6 +6,13 @@ import edu.csuci.appaca.data.statics.StaticClothesItem;
 import edu.csuci.appaca.utils.TimeUtils;
 
 public class HappinessCalc {
+    /**
+     * Calculate happiness depletion based on time, max happiness, value of clothes, and current happiness
+     * @param clothes clothes that the alpaca is currently wearing, used to get value of clothes
+     * @param alpaca current alpaca, used to get current happiness
+     * @param previousTime time since this function was last called
+     * @return new happiness value
+     */
     static public double calcHappiness(StaticClothesItem clothes, Alpaca alpaca, long previousTime)
     {
         final double TIME_TIL_FULLY_DEPLETED = 8 * 60; //8 hours * 60 minutes
