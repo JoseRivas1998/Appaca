d5087a8f2186cfbf7584f17d387077289a5bec64
Forrest Hampton <forrest.hampton744@myci.csuci.edu>
2019-10-25 10:36:36 -0700

    Happiness Depletion done

--- app/src/main/java/edu/csuci/appaca/data/HappinessCalc.java
+++ app/src/main/java/edu/csuci/appaca/data/HappinessCalc.java
@@ -8,12 +8,17 @@ import edu.csuci.appaca.utils.TimeUtils;
 public class HappinessCalc {
     static public double calcHappiness(StaticClothesItem clothes, Alpaca alpaca, long previousTime)
     {
+        final double TIME_TIL_FULLY_DEPLETED = 8 * 60; //8 hours * 60 minutes
+        final double MAXPERCENT = 1.0;
+
         double currentHappiness = alpaca.getHappinessStat();
         double percentHappiness = currentHappiness/Alpaca.MAX_STAT;
 
         long currentTime = TimeUtils.getCurrentTime();
         double timeInMinutes = TimeUtils.secondsToMinutes(currentTime - previousTime);
 
-        return 0.0; //stub
+        double clothesValue = clothes.value;
+
+        return (currentHappiness - (clothesValue * timeInMinutes * (MAXPERCENT - percentHappiness) * (Alpaca.MAX_STAT / TIME_TIL_FULLY_DEPLETED))); //stub
     }
 }
