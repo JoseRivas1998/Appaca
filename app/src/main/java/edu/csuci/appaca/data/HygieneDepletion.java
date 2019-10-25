4c2f6e2d6a77d684d6a5c48d39bc786fce526593
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2019-10-25 12:26:22 -0700

    Hygiene Stat Stub

---
+++ app/src/main/java/edu/csuci/appaca/data/HygieneDepletion.java
@@ -0,0 +1,10 @@
+package edu.csuci.appaca.data;
+
+public class HygieneDepletion {
+
+    public static double hygieneDepletion(Alpaca alpaca, double previousTime) {
+        // TODO see card https://trello.com/c/9S4wpJDt/110-hygiene-depletion
+        return Alpaca.MAX_STAT;
+    }
+
+}
