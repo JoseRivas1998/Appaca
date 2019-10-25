266c790997f6f7b4ca8c92907b74bfa86c97fa53
Forrest Hampton <forrest.hampton744@myci.csuci.edu>
2019-10-25 10:25:42 -0700

    Got time and current happiness percentage

--- app/src/main/java/edu/csuci/appaca/data/HappinessCalc.java
+++ app/src/main/java/edu/csuci/appaca/data/HappinessCalc.java
@@ -1,10 +1,19 @@
 package edu.csuci.appaca.data;
 
+import java.sql.Time;
+
 import edu.csuci.appaca.data.statics.StaticClothesItem;
+import edu.csuci.appaca.utils.TimeUtils;
 
 public class HappinessCalc {
-    static public double calcHappiness(StaticClothesItem clothes, Alpaca alpaca, long currentTime)
+    static public double calcHappiness(StaticClothesItem clothes, Alpaca alpaca, long previousTime)
     {
+        double currentHappiness = alpaca.getHappinessStat();
+        double percentHappiness = currentHappiness/Alpaca.MAX_STAT;
+
+        long currentTime = TimeUtils.getCurrentTime();
+        double timeInMinutes = TimeUtils.secondsToMinutes(currentTime - previousTime);
+
         return 0.0; //stub
     }
 }
