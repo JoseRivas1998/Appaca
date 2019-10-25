a77a46e7dfa4e686f240766a298646dc9464ab57
Forrest Hampton <forrest.hampton744@myci.csuci.edu>
2019-10-25 16:14:56 -0700

    Javadoc

--- app/src/main/java/edu/csuci/appaca/data/HygieneDepletion.java
+++ app/src/main/java/edu/csuci/appaca/data/HygieneDepletion.java
@@ -4,6 +4,13 @@ import edu.csuci.appaca.utils.TimeUtils;
 
 public class HygieneDepletion {
 
+    /**
+     * Depletes hygiene over 16 hours, rate depends on the time since last called and percent of hygiene remaining
+     * @param alpaca current alpaca, used to get current and max hygiene value
+     * @param previousTime time since this function was last called
+     * @return new hygiene value
+     */
+
     public static double hygieneDepletion(Alpaca alpaca, long previousTime) {
         // TODO see card https://trello.com/c/9S4wpJDt/110-hygiene-depletion
         final double TIME_TIL_FULLY_DEPLETED = 16 * 60; //16 hours * 60 minutes
