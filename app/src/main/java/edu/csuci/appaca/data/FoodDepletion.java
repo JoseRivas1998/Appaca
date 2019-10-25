66ce70bbfc51fa934c2b72bccf66ab26c7e9612e
Forrest Hampton <forrest.hampton744@myci.csuci.edu>
2019-10-25 10:40:02 -0700

    Changed finals to double, added final for max percentage

--- app/src/main/java/edu/csuci/appaca/data/FoodDepletion.java
+++ app/src/main/java/edu/csuci/appaca/data/FoodDepletion.java
@@ -11,16 +11,17 @@ public class FoodDepletion {
         @return the new food value for the alpaca
      */
     public static double foodDepletion(Alpaca alpaca, long previousTime) {
-        final int TIME_TIL_FULLY_DEPLETED = 8 * 60; //8 hours * 60 minutes over 4 stages
+        final double TIME_TIL_FULLY_DEPLETED = 8 * 60; //8 hours * 60 minutes over 4 stages
+        final double MAXPERCENTAGE = 1.0; //100%
 
         double alpacaFood = alpaca.getFoodStat();
         long currentTime = TimeUtils.getCurrentTime();
         double timeInMinutes = TimeUtils.secondsToMinutes(currentTime - previousTime);
         double percentFood = (alpacaFood / Alpaca.MAX_STAT);
-        if (percentFood == Alpaca.MAX_STAT) {
+        if (percentFood == MAXPERCENTAGE) { //if at 100% food
             percentFood -= 0.01; //if food is full make it so Alpaca.MAX_STAT - percentfood isn't zero
         }
 
-        return (alpacaFood - (timeInMinutes * (Alpaca.MAX_STAT - percentFood) * Alpaca.MAX_STAT / (TIME_TIL_FULLY_DEPLETED)));
+        return (alpacaFood - (timeInMinutes * (MAXPERCENTAGE - percentFood) * Alpaca.MAX_STAT / (TIME_TIL_FULLY_DEPLETED)));
     }
 }
