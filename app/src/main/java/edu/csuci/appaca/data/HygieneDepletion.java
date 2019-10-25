8f09ba6404519b9253214ffe46ddda0aad185098
Forrest Hampton <forrest.hampton744@myci.csuci.edu>
2019-10-25 12:59:49 -0700

    Started hygiene depletion

--- app/src/main/java/edu/csuci/appaca/data/HygieneDepletion.java
+++ app/src/main/java/edu/csuci/appaca/data/HygieneDepletion.java
@@ -1,9 +1,19 @@
 package edu.csuci.appaca.data;
 
+import edu.csuci.appaca.utils.TimeUtils;
+
 public class HygieneDepletion {
 
-    public static double hygieneDepletion(Alpaca alpaca, double previousTime) {
+    public static double hygieneDepletion(Alpaca alpaca, long previousTime) {
         // TODO see card https://trello.com/c/9S4wpJDt/110-hygiene-depletion
+        final double TIME_TIL_FULLY_DEPLETED = 16 * 60; //16 hours * 60 minutes
+
+        long currentTime = TimeUtils.getCurrentTime();
+        double timeInMinutes = TimeUtils.secondsToMinutes(currentTime - previousTime);
+
+        double alpacaClean = alpaca.getHygieneStat();
+        double percentClean = alpacaClean / Alpaca.MAX_STAT;
+
         return Alpaca.MAX_STAT;
     }
 
