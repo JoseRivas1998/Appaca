0cee4b9af176f72a1521c29ff647b4f2633da14a
Forrest Hampton <forrest.hampton744@myci.csuci.edu>
2019-10-25 10:31:29 -0700

    Javadoc

--- app/src/main/java/edu/csuci/appaca/data/FoodDepletion.java
+++ app/src/main/java/edu/csuci/appaca/data/FoodDepletion.java
@@ -4,8 +4,11 @@ import edu.csuci.appaca.utils.TimeUtils;
 
 public class FoodDepletion {
 
-    /*
-        Alpaca food drain is calculated based on the how much food the alpaca currently has, what is the max food, and
+    /**
+        Alpaca food drain is calculated based on the how much food the alpaca currently has, what is the max food, and the time since the food was last depleted
+        @param alpaca current alpaca
+        @param previousTime time that this function was last called in seconds
+        @return the new food value for the alpaca
      */
     public static double foodDepletion(Alpaca alpaca, long previousTime) {
         final int TIME_TIL_FULLY_DEPLETED = 8 * 60; //8 hours * 60 minutes over 4 stages
