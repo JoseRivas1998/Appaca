5dba614231e33270d85e2b86ad3951bb513dc8cd
teagan <teagan.mail@gmail.com>
2019-10-27 02:49:39 -0700

    Added hunger icon to main screen stats

--- app/src/main/java/edu/csuci/appaca/data/Stat.java
+++ app/src/main/java/edu/csuci/appaca/data/Stat.java
@@ -3,7 +3,7 @@ package edu.csuci.appaca.data;
 import edu.csuci.appaca.R;
 
 public enum Stat {
-    HUNGER(R.drawable.food_stat, R.string.hunger),
+    HUNGER(R.drawable.hunger_icon, R.string.hunger),
     HAPPINESS(R.drawable.happiness_stat, R.string.happiness),
     HYGIENE(R.drawable.hygeine_stat, R.string.hygiene);
     public final int iconId;
