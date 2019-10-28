c65e1ea44112c5ab07c2556bd93d168afa4a931d
Lee <leesolomon808@gmail.com>
2019-10-27 17:45:47 -0700

    Completed Stamina Manager
    Need to test it tho

--- app/src/main/java/edu/csuci/appaca/data/StaminaManager.java
+++ app/src/main/java/edu/csuci/appaca/data/StaminaManager.java
@@ -1,5 +1,6 @@
 package edu.csuci.appaca.data;
 
+import android.content.Context;
 import android.util.Log;
 
 import org.json.JSONArray;
@@ -7,51 +8,75 @@ import org.json.JSONObject;
 import org.json.JSONException;
 
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class StaminaManager {
 
+    private enum StaminaInstance {
+        INSTANCE;
+        int currentValue;
+        int maxValue;
+        boolean loaded;
+        List<Alpaca> alpacas;
 
-    private static void initializeStamina() {
-        try {
-            JSONObject currentStamina = new JSONObject();
-            currentStamina.put("currentStamina", 4);
-            JSONObject maxStamina = new JSONObject();
-            maxStamina.put("maxStamina", 4);
-            SavedTime.setToNow();
-        } catch (JSONException e) {
-            Log.e(StaminaManager.class.getName(), e.getMessage());
+        StaminaInstance() {
+            this.currentValue = 0;
+            this.maxValue = 4;
+            this.loaded = false;
+            this.alpacas = new ArrayList<>();
         }
+
     }
 
-    public static JSONObject increaseMaxStamina(JSONArray alpacas) {
-        int ret;
-        JSONObject retStamina = new JSONObject();
+    private static final String FILENAME = "staminaSaveData.json";
 
+    public static int increaseMaxStamina(JSONArray alpacas) {
+        int ret;
         if (alpacas.length() == 1) {
             ret = 4;
         } else {
             ret = 4 + alpacas.length() - 1;
         }
-        try {
-            retStamina.put("retStamina", ret);
-            return retStamina;
-        } catch (JSONException e) {
-            Log.e(StaminaManager.class.getName(), e.getMessage());
-        }
-        return retStamina;
+
+        return ret;
     }
 
+    public static void saveStamina(Context context) {
+        JSONObject saveData = new JSONObject();
+        try {
+            saveData.put("currentStamina", StaminaInstance.INSTANCE.currentValue);
+            SavedTime.setToNow();
+            saveData.put("savedTime", SavedTime.lastSavedTime());
+        } catch (JSONException e) {
+            Log.e(StaminaManager.class.getName(), e.getMessage(), e);
+        }
+        try (FileOutputStream fo = context.openFileOutput(FILENAME, Context.MODE_PRIVATE);
+             OutputStreamWriter osw = new OutputStreamWriter(fo)) {
+            osw.write(saveData.toString());
+        } catch (IOException e) {
+            Log.e(StaminaManager.class.getName(), e.getMessage(), e);
+        }
+    }
+
+    public static boolean load(JSONObject jsonObject) throws JSONException {
+        if (StaminaManager.StaminaInstance.INSTANCE.loaded) return true;
+
+        if (!jsonObject.has("stamina")) throw new JSONException("Stamina is missing!");
+
+        JSONArray alpacas = jsonObject.getJSONArray("alpacas");
+        for (int i = 0; i < alpacas.length(); i++) {
+            StaminaManager.StaminaInstance.INSTANCE.alpacas.add(Alpaca.ofJSON(alpacas.getJSONObject(i)));
+        }
+        StaminaInstance.INSTANCE.currentValue = increaseMaxStamina(alpacas);
+
+        StaminaInstance.INSTANCE.loaded = true;
+
+        return StaminaInstance.INSTANCE.loaded;
+    }
 
 }
-
-
-
-
-//TODO: Loading, Saving
-    //i need initialization, load, saving, and caculating the amount of stamina if the user has more than one alpaca.
-
-
-
-/**
- * This includes saving the max stamina, the current stamina, and the time that the first stamina was used.
- **/
