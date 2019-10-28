140171f324f486d941e829a062cd8dfc098cecaf
Lee <leesolomon808@gmail.com>
2019-10-28 13:54:36 -0700

    Finished up the Stamina loading and saving

--- app/src/main/java/edu/csuci/appaca/data/StaminaManager.java
+++ app/src/main/java/edu/csuci/appaca/data/StaminaManager.java
@@ -1,20 +1,7 @@
 package edu.csuci.appaca.data;
 
-import android.content.Context;
-import android.util.Log;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.json.JSONException;
-
-
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.List;
-
-import edu.csuci.appaca.utils.TimeUtils;
+import org.json.JSONObject;
 
 import static edu.csuci.appaca.utils.TimeUtils.getCurrentTime;
 
@@ -33,60 +20,50 @@ public class StaminaManager {
             this.maxValue = 0;
             this.loaded = false;
             // just initialize it to zero
-            this.firstStaminaUsedTime = getCurrentTime(); //idk why thats not working lol
+            this.firstStaminaUsedTime = getCurrentTime();
         }
 
     }
 
-    private static final String FILENAME = "staminaSaveData.json";
+    public static final int MAX_STAMINA = 4;
 
-    public static int increaseMaxStamina() {
-        // this whole method can be a one liner
-        // StaminaInstance.INSTANCE.currentValue++
-        int ret = StaminaInstance.INSTANCE.currentValue + 1;
+    public static void increaseMaxStamina() {
+        StaminaInstance.INSTANCE.currentValue++;
 
-        return ret;
     }
 
     public static JSONObject toJSONObject() throws JSONException {
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("currentStamina", StaminaInstance.INSTANCE.currentValue);
         jsonObject.put("maxValue", StaminaInstance.INSTANCE.maxValue);
-        jsonObject.put("loaded", StaminaInstance.INSTANCE.loaded); // dont include this
-        // include firstStaminaUsedTime
-
+        jsonObject.put("firstStaminaUsedTime", StaminaInstance.INSTANCE.firstStaminaUsedTime);
         return jsonObject;
     }
 
-    public static boolean load(JSONObject jsonObject) throws JSONException {
-        if (StaminaManager.StaminaInstance.INSTANCE.loaded) return true;
+    public static void load(JSONObject jsonObject) throws JSONException {
+        if (StaminaManager.StaminaInstance.INSTANCE.loaded) return;
+        try {
+            // we are assuming that the object we are accepting  to begin with
+            if (!jsonObject.has("currentStamina")) throw new JSONException("Stamina is missing");
+            if (!jsonObject.has("maxValue")) throw new JSONException("maxValue is missing");
+            if (!jsonObject.has("firstStaminaUsedTime"))
+                throw new JSONException("firstStaminaUsedTime is missing");
 
-        // we are assuming that the object we are accepting  to begin with
-        if (!jsonObject.has("stamina")) throw new JSONException("Stamina is missing!");
-        // Here is the JSON that this method should be expecting
-        /*
-         * {
-         *      "currentStamina": ##,
-         *      "maxValue": ##,
-         *      "firstStaminaUsedTime": ####
-         * }
-         */
-        // We are simply copying the values from the json object to the enum
-        // Here is a sample of grabbing a thing
-        // StaminaInstance.INSTANCE.currentValue = jsonObject.getInt("currentValue");
+            // StaminaInstance.INSTANCE.currentValue = jsonObject.getInt("currentValue");
 
+            StaminaInstance.INSTANCE.currentValue = jsonObject.getInt("currenValue");
+            StaminaInstance.INSTANCE.maxValue = jsonObject.getInt("maxValue");
+            StaminaInstance.INSTANCE.maxValue = jsonObject.getInt("firstStaminaUsedTime");
+        } catch (JSONException je) {
+            init();
+        }
         StaminaInstance.INSTANCE.loaded = true;
-
-        return StaminaInstance.INSTANCE.loaded;
     }
 
-    /*
-     * We need a static method called init:
-     *  if loaded is true, return
-     *  set max stamina to 4 (use a static constant)
-     *  set current stamina = 4
-     *  set firstStaminaUsedTime to 0 (this value is only relevant when current stamina != max stamina
-     *  set loaded to true
-     */
+    public static void init() {
+        StaminaInstance.INSTANCE.maxValue = MAX_STAMINA;
+        StaminaInstance.INSTANCE.currentValue = MAX_STAMINA;
+        StaminaInstance.INSTANCE.firstStaminaUsedTime = 0;
+    }
 
 }
