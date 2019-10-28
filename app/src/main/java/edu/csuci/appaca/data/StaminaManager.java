5bc7d92e928ddbce7c8f90864c48cc51734d0393
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2019-10-27 21:32:43 -0700

    Comments
    Hope this helps!

--- app/src/main/java/edu/csuci/appaca/data/StaminaManager.java
+++ app/src/main/java/edu/csuci/appaca/data/StaminaManager.java
@@ -30,8 +30,9 @@ public class StaminaManager {
 
         StaminaInstance() {
             this.currentValue = 0;
-            this.maxValue = 4;
+            this.maxValue = 0;
             this.loaded = false;
+            // just initialize it to zero
             this.firstStaminaUsedTime = getCurrentTime(); //idk why thats not working lol
         }
 
@@ -40,7 +41,8 @@ public class StaminaManager {
     private static final String FILENAME = "staminaSaveData.json";
 
     public static int increaseMaxStamina() {
-
+        // this whole method can be a one liner
+        // StaminaInstance.INSTANCE.currentValue++
         int ret = StaminaInstance.INSTANCE.currentValue + 1;
 
         return ret;
@@ -50,7 +52,8 @@ public class StaminaManager {
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("currentStamina", StaminaInstance.INSTANCE.currentValue);
         jsonObject.put("maxValue", StaminaInstance.INSTANCE.maxValue);
-        jsonObject.put("loaded", StaminaInstance.INSTANCE.loaded);
+        jsonObject.put("loaded", StaminaInstance.INSTANCE.loaded); // dont include this
+        // include firstStaminaUsedTime
 
         return jsonObject;
     }
@@ -58,16 +61,32 @@ public class StaminaManager {
     public static boolean load(JSONObject jsonObject) throws JSONException {
         if (StaminaManager.StaminaInstance.INSTANCE.loaded) return true;
 
+        // we are assuming that the object we are accepting  to begin with
         if (!jsonObject.has("stamina")) throw new JSONException("Stamina is missing!");
-//
-//        JSONArray alpacas = jsonObject.getJSONArray("alpacas");
-//        for (int i = 0; i < alpacas.length(); i++) {
-//        }
-//        StaminaInstance.INSTANCE.currentValue = increaseMaxStamina(alpacas);
+        // Here is the JSON that this method should be expecting
+        /*
+         * {
+         *      "currentStamina": ##,
+         *      "maxValue": ##,
+         *      "firstStaminaUsedTime": ####
+         * }
+         */
+        // We are simply copying the values from the json object to the enum
+        // Here is a sample of grabbing a thing
+        // StaminaInstance.INSTANCE.currentValue = jsonObject.getInt("currentValue");
 
         StaminaInstance.INSTANCE.loaded = true;
 
         return StaminaInstance.INSTANCE.loaded;
     }
+
+    /*
+     * We need a static method called init:
+     *  if loaded is true, return
+     *  set max stamina to 4 (use a static constant)
+     *  set current stamina = 4
+     *  set firstStaminaUsedTime to 0 (this value is only relevant when current stamina != max stamina
+     *  set loaded to true
+     */
 
 }
