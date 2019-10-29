c4dcfaa78fb3b18040e90728fb48a367eba34fb7
Lee <leesolomon808@gmail.com>
2019-10-28 22:13:19 -0700

    Worked on the Minigame Dynamic Selection
    Needs to be tested though!

--- app/src/main/java/edu/csuci/appaca/activities/MinigameSelectActivity.java
+++ app/src/main/java/edu/csuci/appaca/activities/MinigameSelectActivity.java
@@ -2,35 +2,42 @@ package edu.csuci.appaca.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.view.View;
-import android.content.Intent;
+import android.widget.GridLayout;
+import android.widget.ImageView;
 
 import edu.csuci.appaca.R;
 import edu.csuci.appaca.data.MiniGames;
+import edu.csuci.appaca.utils.ScreenUtils;
 
 public class MinigameSelectActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        GridLayout gridLayout = new GridLayout(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_minigame_select);
         getSupportActionBar().hide();
+        int size = (int) ScreenUtils.dpToPixels(this, 400);
+        int margin = (int) ScreenUtils.dpToPixels(this, 30);
 
-        for (MiniGames miniGame : MiniGames.values())
-        {
 
+        for (MiniGames miniGame : MiniGames.values()) {
+            int id = miniGame.iconId;
+            ImageView gameView = new ImageView(this);
+            gameView.setImageResource(miniGame.iconId);
+
+            GridLayout.LayoutParams params = new GridLayout.LayoutParams();
+            params.width = size;
+            params.height = size;
+            params.setMargins(margin, margin, margin, margin);
+            gameView.setLayoutParams(params);
+
+            gridLayout.addView(gameView);
         }
+
+
     }
 
 
-
 }
-
-//    terate through minigames enum in mingamesjava.
-//
-//        fo each minigame, create an image view where i set the drawale to the value's icon id.
-//        set the onclick to witch to cactivity clas
