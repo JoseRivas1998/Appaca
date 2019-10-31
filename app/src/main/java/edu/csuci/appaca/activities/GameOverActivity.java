dcc399c6ab1744e1ef98a73078a71278fb2f6880
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2019-10-31 12:02:08 -0700

    Display high score in game over

--- app/src/main/java/edu/csuci/appaca/activities/GameOverActivity.java
+++ app/src/main/java/edu/csuci/appaca/activities/GameOverActivity.java
@@ -12,20 +12,46 @@ import android.widget.TextView;
 import com.badlogic.gdx.Game;
 
 import edu.csuci.appaca.R;
+import edu.csuci.appaca.data.HighScore;
 import edu.csuci.appaca.data.MiniGames;
+import edu.csuci.appaca.data.SaveDataUtils;
 
 public class GameOverActivity extends AppCompatActivity {
 
     private MiniGames returnTo;
     private int score;
+    private int highScore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_over);
         loadIntentData();
+        updateSaveData();
         getSupportActionBar().hide();
         initButton();
+    }
+
+    private void updateSaveData() {
+        updateHighScore();
+        updateCurrencyAlpacas();
+        updateHappiness();
+        SaveDataUtils.updateValuesAndSave(this);
+    }
+
+    private void updateCurrencyAlpacas() {
+        // TODO this is a stub
+    }
+
+    private void updateHappiness() {
+        // TODO this is a stub
+    }
+
+    private void updateHighScore() {
+        this.highScore = HighScore.getHighScore(returnTo);
+        if(HighScore.putHighScore(returnTo, this.score)) {
+            this.highScore = this.score;
+        }
     }
 
     private void loadIntentData() {
@@ -39,8 +65,11 @@ public class GameOverActivity extends AppCompatActivity {
         final Button exitButton = findViewById(R.id.exitButton);
         final Button playAgainButton = findViewById(R.id.playAgain);
         final TextView scoreText = findViewById(R.id.scoreText);
+        final TextView highScoreText = findViewById(R.id.highScoreText);
 
         scoreText.setText(String.format(getString(returnTo.scoreFormatId), this.score));
+
+        highScoreText.setText(String.format(getString(returnTo.highScoreFormatId), this.score));
 
         exitButton.setOnClickListener(new View.OnClickListener() {
             @Override
