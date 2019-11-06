8fca0c33224d35ce698c26588338ea07f6da9b75
Forrest Hampton <forrest.hampton744@myci.csuci.edu>
2019-11-06 15:35:45 -0800

    Added functions

--- app/src/main/java/edu/csuci/appaca/notifications/HygieneNotification.java
+++ app/src/main/java/edu/csuci/appaca/notifications/HygieneNotification.java
@@ -1,4 +1,16 @@
 package edu.csuci.appaca.notifications;
 
+import edu.csuci.appaca.data.Alpaca;
+import edu.csuci.appaca.data.AlpacaFarm;
+import edu.csuci.appaca.utils.ListUtils;
+
 public class HygieneNotification {
+    public static void checkIfAnyAlpacasLowHygiene() {
+
+    }
+
+    public static void sendNotification() {
+
+    }
+
 }
