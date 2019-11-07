f46305b4f3245c3cd8af31f1ee732122f3140c77
Forrest Hampton <forrest.hampton744@myci.csuci.edu>
2019-11-07 10:40:38 -0800

    Added objects needed for notifications, made class abstract

--- app/src/main/java/edu/csuci/appaca/notifications/HygieneNotification.java
+++ app/src/main/java/edu/csuci/appaca/notifications/HygieneNotification.java
@@ -1,10 +1,24 @@
 package edu.csuci.appaca.notifications;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+
 import edu.csuci.appaca.data.Alpaca;
 import edu.csuci.appaca.data.AlpacaFarm;
 import edu.csuci.appaca.utils.ListUtils;
 
-public class HygieneNotification {
+abstract public class HygieneNotification extends AppCompatActivity {
+    private final String NOTIFICATION_CHANNEL_ID = "hygiene_id";
+    private NotificationCompat.Builder builder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+    private NotificationManager notificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+
     public static void checkIfAnyAlpacasLowHygiene() {
 
         AlpacaFarm.forEach(new ListUtils.Consumer<Alpaca>() {
