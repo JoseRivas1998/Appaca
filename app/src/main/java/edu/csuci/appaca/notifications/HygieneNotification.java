48547accc4156954eb15b30955e84fa71e89512f
Forrest Hampton <forrest.hampton744@myci.csuci.edu>
2019-11-07 15:08:43 -0800

    Implemented notification

--- app/src/main/java/edu/csuci/appaca/notifications/HygieneNotification.java
+++ app/src/main/java/edu/csuci/appaca/notifications/HygieneNotification.java
@@ -4,6 +4,8 @@ import android.app.NotificationManager;
 import android.content.Context;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
 import edu.csuci.appaca.data.Alpaca;
 import edu.csuci.appaca.data.AlpacaFarm;
 import edu.csuci.appaca.utils.ListUtils;
@@ -26,8 +28,16 @@ abstract public class HygieneNotification extends AppCompatActivity {
     public static void sendNotification(Context context, String alpacaName)
     {
         final String NOTIFICATION_CHANNEL_ID = "hygiene_id";
+        final String GROUP_ID = "stat_group";
+        final int NOTIFY_ID = 0;
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID);
-        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+
+        builder.setContentTitle("Appaca");
+        builder.setContentText(alpacaName + "is dirty!");
+        builder.setGroup(GROUP_ID);
+
+        notificationManager.notify(NOTIFY_ID, builder.build());
     }
 
 }
