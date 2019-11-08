d17ae2b8ea3f45b36bf6c072d5c22e9d1bbc0e29
Forrest Hampton <forrest.hampton744@myci.csuci.edu>
2019-11-08 10:49:53 -0800

    Tapping notification opens app, cleaned up redundant code

--- app/src/main/java/edu/csuci/appaca/notifications/HygieneNotification.java
+++ app/src/main/java/edu/csuci/appaca/notifications/HygieneNotification.java
@@ -2,13 +2,15 @@ package edu.csuci.appaca.notifications;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
 
 import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
 
 import edu.csuci.appaca.R;
+import edu.csuci.appaca.activities.MainActivity;
 import edu.csuci.appaca.data.Alpaca;
 import edu.csuci.appaca.data.AlpacaFarm;
 import edu.csuci.appaca.utils.ListUtils;
@@ -33,13 +35,14 @@ public class HygieneNotification{
         final String CHANNEL_ID = "hygiene_id";
         final String GROUP_ID = "stat_group";
         final int NOTIFY_ID = 0;
+        Intent toMainScreen = new Intent(context, MainActivity.class);
+        PendingIntent notificationIntent = PendingIntent.getActivity(context, 0, toMainScreen, 0);
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID);
         NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
 
         if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
         {
             CharSequence name = "hygiene";
-            String description = "Hygiene channel";
             int importance= NotificationManager.IMPORTANCE_HIGH;
             NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
             notificationManager.createNotificationChannel(channel);
@@ -50,6 +53,7 @@ public class HygieneNotification{
         builder.setGroup(GROUP_ID);
         builder.setSmallIcon(R.drawable.alpaca_icon); //placeholder
         builder.setOnlyAlertOnce(true);
+        builder.setContentIntent(notificationIntent);
 
         notificationManager.notify(NOTIFY_ID, builder.build());
     }
