0263a262c3c8c7eda0eb3ccd24d0ffbbd993a7c9
Forrest Hampton <forrest.hampton744@myci.csuci.edu>
2019-11-08 16:55:35 -0800

    Added class for checking hygiene

---
+++ app/src/main/java/edu/csuci/appaca/notifications/NotificationChecker.java
@@ -0,0 +1,19 @@
+package edu.csuci.appaca.notifications;
+
+import android.content.Context;
+
+public class NotificationChecker implements Runnable
+{
+    private Context context;
+
+    public NotificationChecker(Context context)
+    {
+        super();
+        this.context = context;
+    }
+
+    @Override
+    public void run() {
+        HygieneNotification.checkIfAnyAlpacasLowHygiene(context);
+    }
+}
