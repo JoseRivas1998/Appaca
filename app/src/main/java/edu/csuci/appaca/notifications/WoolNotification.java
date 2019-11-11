5945fe2f718ed666ef19f4a430c7ba1186f2bac9
Forrest Hampton <forrest.hampton744@myci.csuci.edu>
2019-11-10 17:55:05 -0800

    Started notification function

--- app/src/main/java/edu/csuci/appaca/notifications/WoolNotification.java
+++ app/src/main/java/edu/csuci/appaca/notifications/WoolNotification.java
@@ -1,6 +1,9 @@
 package edu.csuci.appaca.notifications;
 
+import android.app.NotificationManager;
 import android.content.Context;
+
+import androidx.core.app.NotificationCompat;
 
 import edu.csuci.appaca.data.Alpaca;
 import edu.csuci.appaca.data.AlpacaFarm;
@@ -23,5 +26,12 @@ public class WoolNotification {
         });
     }
 
-    public static void sendNotification(Context context, String alpacaName){}
+    private static void sendNotification(Context context, String alpacaName){
+        //send notification saying that alpaca is ready to shear
+        final String CHANNEL_ID = "wool_id";
+        final String GROUP_ID = "stat_group";
+        final int NOTIFY_ID = 1;
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID);
+        NotificationManager manager = context.getSystemService(NotificationManager.class);
+    }
 }
