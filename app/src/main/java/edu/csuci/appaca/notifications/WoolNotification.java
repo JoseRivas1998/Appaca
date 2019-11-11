aba87ee61e01293c74ac29619c3c9b1145304181
Forrest Hampton <forrest.hampton744@myci.csuci.edu>
2019-11-10 17:21:59 -0800

    Created class and methods

---
+++ app/src/main/java/edu/csuci/appaca/notifications/WoolNotification.java
@@ -0,0 +1,10 @@
+package edu.csuci.appaca.notifications;
+
+import android.content.Context;
+
+public class WoolNotification {
+
+    public static void checkIfAnyAlpacasMaxWool(Context context){}
+
+    public static void sendNotification(Context context, String alpacaName){}
+}
