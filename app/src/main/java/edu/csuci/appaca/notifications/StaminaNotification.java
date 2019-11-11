7c897e779ccf84c008b48c5b28c373021cc48707
Forrest Hampton <forrest.hampton744@myci.csuci.edu>
2019-11-11 13:34:47 -0800

    Created class and functions

---
+++ app/src/main/java/edu/csuci/appaca/notifications/StaminaNotification.java
@@ -0,0 +1,10 @@
+package edu.csuci.appaca.notifications;
+
+import android.content.Context;
+
+public class StaminaNotification {
+
+    public static void checkIfStaminaIsFull(Context context){}
+
+    private static void sendNotification(Context context){}
+}
