caba418819b8478d5e5d571cdcb25464fc93975a
Forrest Hampton <forrest.hampton744@myci.csuci.edu>
2019-11-11 13:36:19 -0800

    Implemented stamina check

--- app/src/main/java/edu/csuci/appaca/notifications/StaminaNotification.java
+++ app/src/main/java/edu/csuci/appaca/notifications/StaminaNotification.java
@@ -2,9 +2,17 @@ package edu.csuci.appaca.notifications;
 
 import android.content.Context;
 
+import edu.csuci.appaca.data.StaminaManager;
+
 public class StaminaNotification {
 
-    public static void checkIfStaminaIsFull(Context context){}
+    public static void checkIfStaminaIsFull(Context context){
+        int currentStamina = StaminaManager.getCurrentStamina();
+        if (currentStamina == StaminaManager.MAX_STAMINA)
+        {
+            sendNotification(context);
+        }
+    }
 
     private static void sendNotification(Context context){}
 }
