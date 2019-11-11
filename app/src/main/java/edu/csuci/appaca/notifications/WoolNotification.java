36cb3c9ebfc1dce39dff0a374fc8bf8471a5a082
Forrest Hampton <forrest.hampton744@myci.csuci.edu>
2019-11-10 17:50:20 -0800

    Implemented wool check

--- app/src/main/java/edu/csuci/appaca/notifications/WoolNotification.java
+++ app/src/main/java/edu/csuci/appaca/notifications/WoolNotification.java
@@ -2,9 +2,26 @@ package edu.csuci.appaca.notifications;
 
 import android.content.Context;
 
+import edu.csuci.appaca.data.Alpaca;
+import edu.csuci.appaca.data.AlpacaFarm;
+import edu.csuci.appaca.utils.ListUtils;
+import edu.csuci.appaca.utils.ShearUtils;
+
 public class WoolNotification {
 
-    public static void checkIfAnyAlpacasMaxWool(Context context){}
+    public static void checkIfAnyAlpacasMaxWool(final Context context){
+        AlpacaFarm.forEach(new ListUtils.Consumer<Alpaca>() {
+            @Override
+            public void accept(Alpaca alpaca) {
+                final int MAX_MONEY = 50;
+                int money = ShearUtils.getShearValue(alpaca, context);
+                if (money == MAX_MONEY)
+                {
+                    sendNotification(context, alpaca.getName());
+                }
+            }
+        });
+    }
 
     public static void sendNotification(Context context, String alpacaName){}
 }
