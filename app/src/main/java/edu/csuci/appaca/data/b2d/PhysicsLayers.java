0c37857e584b1c23f73db33b32b9c440f5383bd8
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2019-11-12 10:21:02 -0800

    Breakable platforms

--- app/src/main/java/edu/csuci/appaca/data/b2d/PhysicsLayers.java
+++ app/src/main/java/edu/csuci/appaca/data/b2d/PhysicsLayers.java
@@ -6,5 +6,6 @@ public final class PhysicsLayers {
     public static final short ALPACA = 1 << 1;
     public static final short ALPACA_FOOT = 1 << 2;
     public static final short SPRING = 1 << 3;
+    public static final short PLATFORM_BREAKABLE = 1 << 4;
 
 }
--- app/src/main/java/edu/csuci/appaca/data/content/StaticContentManager.java
+++ app/src/main/java/edu/csuci/appaca/data/content/StaticContentManager.java
@@ -69,7 +69,8 @@ public class StaticContentManager {
         HEART("heart.png"),
         MAIN_SCREEN_BG("backgrounds/appacabackground.png"),
         SPRING_DOWN("alpaca-jump/img/spring_down.png"),
-        SPRING_EXTENDED("alpaca-jump/img/spring-extended.png");
+        SPRING_EXTENDED("alpaca-jump/img/spring-extended.png"),
+        PLATFORM_BREAKABLE("alpaca-jump/img/platform_breakable.png");
         final String path;
 
         Image(String path) {
