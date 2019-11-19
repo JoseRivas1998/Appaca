b1fb3ff8fc0a7630ac06a3ecf9aeb576f71296c1
teagan <teagan.mail@gmail.com>
2019-11-19 11:55:11 -0800

    show all potential and current clothes and remove button

--- app/src/main/java/edu/csuci/appaca/activities/ClothingSelectActivity.java
+++ app/src/main/java/edu/csuci/appaca/activities/ClothingSelectActivity.java
@@ -2,6 +2,7 @@ package edu.csuci.appaca.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +13,7 @@ import android.widget.ImageView;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 
 import edu.csuci.appaca.R;
+import edu.csuci.appaca.data.Alpaca;
 import edu.csuci.appaca.data.AlpacaFarm;
 import edu.csuci.appaca.data.Inventory;
 import edu.csuci.appaca.data.SaveDataUtils;
@@ -42,11 +44,23 @@ public class ClothingSelectActivity extends AndroidApplication {
     }
 
     private void loadClothes() {
+        IconBadgeFragment remove = IconBadgeFragment.ofResource(R.drawable.x_2, "Remove");
+        // TODO 144 load preview instead of doing this
+        remove.setOnIconClick(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                confirmClothes(Alpaca.NO_CLOTHING);
+            }
+        });
+        getFragmentManager()
+                .beginTransaction()
+                .add(R.id.clothing_select_list, remove)
+                .commit();
+
         for (final StaticClothesItem clothesItem : ShopData.getAllClothes()) {
             int amount = Inventory.getClothesAmount(clothesItem.id);
-            if (amount > 0) {
+            if (amount > 0 || AlpacaFarm.getCurrentAlpaca().getClothing() == clothesItem.id) {
                 IconBadgeFragment iconBadge = IconBadgeFragment.ofAsset(clothesItem.path, String.valueOf(amount));
-                // TODO 144 load preview instead of doing this
                 iconBadge.setOnIconClick(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
@@ -62,11 +76,13 @@ public class ClothingSelectActivity extends AndroidApplication {
     }
 
     private void confirmClothes(int clothesId) {
-        SaveDataUtils.updateValuesAndSave(ClothingSelectActivity.this);
-        Inventory.useClothes(clothesId);
-        AlpacaFarm.getCurrentAlpaca().setClothing(clothesId);
-        SaveDataUtils.save(ClothingSelectActivity.this);
-        finish();
+        if (AlpacaFarm.getCurrentAlpaca().getClothing() != clothesId) {
+            SaveDataUtils.updateValuesAndSave(ClothingSelectActivity.this);
+            Inventory.useClothes(clothesId);
+            AlpacaFarm.getCurrentAlpaca().setClothing(clothesId);
+            SaveDataUtils.save(ClothingSelectActivity.this);
+            //finish();
+        }
     }
 
 }
