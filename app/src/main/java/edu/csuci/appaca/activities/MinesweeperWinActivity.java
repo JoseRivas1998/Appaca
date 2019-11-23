d9bc52179a54981b9ad8fd5a02e7251c098d60a3
Forrest Hampton <forrest.hampton744@myci.csuci.edu>
2019-11-23 08:30:33 -0800

    Extended game over activity, using game over layout as a base

--- app/src/main/java/edu/csuci/appaca/activities/MinesweeperWinActivity.java
+++ app/src/main/java/edu/csuci/appaca/activities/MinesweeperWinActivity.java
@@ -6,11 +6,16 @@ import android.os.Bundle;
 
 import edu.csuci.appaca.R;
 
-public class MinesweeperWinActivity extends AppCompatActivity {
+public class MinesweeperWinActivity extends GameOverActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_minesweeper_win);
+    }
+
+    @Override
+    public void initButton() {
+        super.initButton();
     }
 }
