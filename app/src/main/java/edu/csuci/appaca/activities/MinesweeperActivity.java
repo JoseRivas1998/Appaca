0152059090aa62129bb1e5a81a1d9b809bebc0d9
Forrest Hampton <forrest.hampton744@myci.csuci.edu>
2019-11-25 14:26:19 -0800

    Added minesweeper to minigames

--- app/src/main/java/edu/csuci/appaca/activities/MinesweeperActivity.java
+++ app/src/main/java/edu/csuci/appaca/activities/MinesweeperActivity.java
@@ -2,20 +2,22 @@ package edu.csuci.appaca.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
 
 import edu.csuci.appaca.R;
 
 public class MinesweeperActivity extends AppCompatActivity {
+    private final int GRID_SIZE = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initMinesweeper();
         setContentView(R.layout.activity_minesweeper);
+    }
+
+    private void initMinesweeper() {
+
     }
 
 }
