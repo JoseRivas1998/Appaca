473b03c957959ce4181d36c54faaa0ddfb33c585
Forrest Hampton <forrest.hampton744@myci.csuci.edu>
2019-11-26 12:53:58 -0800

    Tiles drawn to screen

--- app/src/main/java/edu/csuci/appaca/activities/MinesweeperActivity.java
+++ app/src/main/java/edu/csuci/appaca/activities/MinesweeperActivity.java
@@ -6,22 +6,30 @@ import android.os.Bundle;
 import android.widget.GridLayout;
 
 import edu.csuci.appaca.R;
-import edu.csuci.appaca.data.MinesweeperTile;
+import edu.csuci.appaca.graphics.minesweeper.MinesweeperTile;
 
 public class MinesweeperActivity extends AppCompatActivity {
-    private final int GRID_SIZE = 5*5;
+    private final int GRID_SIZE = 16;
+    private MinesweeperTile[][] grid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        initMinesweeper();
         setContentView(R.layout.activity_minesweeper);
+        initMinesweeper();
+        getSupportActionBar().hide();
     }
 
     private void initMinesweeper() {
-        GridLayout grid = findViewById(R.id.minesweeper_grid);
-        for (int tileNum = 1; tileNum <= GRID_SIZE; tileNum++) {
-            MinesweeperTile tile = MinesweeperTile.createTile(tileNum, this.getApplicationContext());
+        grid = new MinesweeperTile[GRID_SIZE][GRID_SIZE];
+        GridLayout view = findViewById(R.id.minesweeper_grid);
+        view.setColumnCount(GRID_SIZE);
+        for(int i = 0; i < GRID_SIZE; i++) {
+            for(int j = 0; j < GRID_SIZE; j++) {
+                MinesweeperTile tile = MinesweeperTile.createTile(i+1, j+1, GRID_SIZE, this.getApplicationContext());
+                grid[i][j] = tile;
+                view.addView(tile.view);
+            }
         }
     }
 
