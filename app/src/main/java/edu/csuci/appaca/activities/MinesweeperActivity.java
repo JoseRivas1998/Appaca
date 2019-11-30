eb876b42b55ed4da0a398ecd6b866ed65184e861
Forrest Hampton <forrest.hampton744@myci.csuci.edu>
2019-11-30 12:56:21 -0800

    Go to win sceeen when the number of revealed tiles is at max

--- app/src/main/java/edu/csuci/appaca/activities/MinesweeperActivity.java
+++ app/src/main/java/edu/csuci/appaca/activities/MinesweeperActivity.java
@@ -20,7 +20,7 @@ public class MinesweeperActivity extends AppCompatActivity {
     private int score = 0;
     private long timePlayed = 0;
 
-    private final int numSafeTiles = (GRID_SIZE * GRID_SIZE) - MAX_BOMBS;
+    private final int NUM_SAFE_TILES = (GRID_SIZE * GRID_SIZE) - MAX_BOMBS;
     public static int tilesRevealed = 0;
 
     @Override
@@ -52,6 +52,7 @@ public class MinesweeperActivity extends AppCompatActivity {
                         {
                             revealNeighboringTiles(tile.row, tile.column);
                         }
+                        checkWin();
                     }
                 });
                 grid[i][j] = tile;
@@ -61,7 +62,9 @@ public class MinesweeperActivity extends AppCompatActivity {
     }
 
     private void checkWin(){
-
+        if(tilesRevealed == NUM_SAFE_TILES) {
+            MiniGames.winGame(MinesweeperActivity.this, MiniGames.MINESWEEPER, score, timePlayed);
+        }
     }
 
     private void placeBombs() {
