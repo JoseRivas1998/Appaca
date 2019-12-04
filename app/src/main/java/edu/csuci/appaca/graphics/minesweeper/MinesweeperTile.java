17319663495f3f45f58be4ee5b84d434bd25d734
teagan <teagan.mail@gmail.com>
2019-12-04 12:50:28 -0800

    Changed tiles from view to textView

--- app/src/main/java/edu/csuci/appaca/graphics/minesweeper/MinesweeperTile.java
+++ app/src/main/java/edu/csuci/appaca/graphics/minesweeper/MinesweeperTile.java
@@ -3,6 +3,7 @@ package edu.csuci.appaca.graphics.minesweeper;
 import android.content.Context;
 import android.view.View;
 import android.widget.GridLayout;
+import android.widget.TextView;
 
 import edu.csuci.appaca.R;
 import edu.csuci.appaca.activities.MinesweeperActivity;
@@ -14,20 +15,21 @@ public class MinesweeperTile {
     public int row;
     public int column;
     public boolean bomb;
-    public View view;
+    public TextView view;
 
     public  static MinesweeperTile createTile(int row, int column, int count, final Context context) {
         int size = (int) ScreenUtils.dpToPixels(context, (float)(2100/(count + 5 *(count - 1))));
         int margin = (int) ScreenUtils.dpToPixels(context, 1);
 
         MinesweeperTile ret = new MinesweeperTile();
-        ret.view = new View(context);
+        ret.view = new TextView(context);
         ret.view.setBackgroundColor(context.getColor(R.color.bluePastel));
         GridLayout.LayoutParams params = new GridLayout.LayoutParams();
         params.width = size;
         params.height = size;
         params.setMargins(margin, margin, margin, margin);
         ret.view.setLayoutParams(params);
+        ret.view.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
 
         ret.row = row;
         ret.column = column;
