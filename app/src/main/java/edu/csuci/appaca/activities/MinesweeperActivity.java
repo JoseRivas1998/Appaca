43dbd3dd731adf9c9a5381f74d6e29509555a92e
teagan <teagan.mail@gmail.com>
2019-12-13 00:01:03 -0800

    sets both types of uncovered tiles

--- app/src/main/java/edu/csuci/appaca/activities/MinesweeperActivity.java
+++ app/src/main/java/edu/csuci/appaca/activities/MinesweeperActivity.java
@@ -154,6 +154,9 @@ public class MinesweeperActivity extends AppCompatActivity {
             if (revealed) {
                 setNumOfNearMinesString(numOfBombs);
                 grid[y][x].view.setText(buffer);
+                if (numOfBombs > 0) {
+                    grid[y][x].view.setBackground(AssetsUtils.drawableFromAsset(this.getApplicationContext(), "minesweeper/uncovered_tile_with_text.png"));
+                }
                 switch (numOfBombs) {
                     case 1:
                         grid[y][x].view.setTextColor(getColor(R.color.minesweeperone));
